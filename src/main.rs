d5325074325c1583e8d0810aef7114531c8d2b93
martinthenext <martin.davtyan@gmail.com>
2023-12-10 21:29:02 +0000

    This one time dice bot worked

--- src/main.rs
+++ src/main.rs
@@ -1,31 +1,15 @@
-use axum::{extract::Query, http::StatusCode, routing::get, Json, Router};
-use serde::Serialize;
-use std::collections::HashMap;
-
-#[derive(Serialize)]
-struct Msg {
-    message: String,
-}
+use teloxide::prelude::*;
 
 #[tokio::main]
 async fn main() {
-    let app = Router::new().route("/", get(handler));
+    pretty_env_logger::init();
+    log::info!("Starting throw dice bot...");
 
-    println!("Rust REST service started...");
-    axum::Server::bind(&"0.0.0.0:3000".parse().unwrap())
-        .serve(app.into_make_service())
-        .await
-        .unwrap();
-}
+    let bot = Bot::from_env();
 
-async fn handler(Query(params): Query<HashMap<String, String>>) -> (StatusCode, Json<Msg>) {
-    let name = match params.get("name") {
-        Some(value) => value,
-        None => "World",
-    };
-    let msg = Msg {
-        message: format!("Hello {}!", name.trim()),
-    };
-
-    (StatusCode::OK, Json(msg))
+    teloxide::repl(bot, |bot: Bot, msg: Message| async move {
+        bot.send_dice(msg.chat.id).await?;
+        Ok(())
+    })
+    .await;
 }
