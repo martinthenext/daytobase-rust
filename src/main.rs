dd549bb4462383fe0219ddd683b542e5e748ba4e
martinthenext <martin.davtyan@gmail.com>
2023-12-10 23:29:45 +0000

    Webhooks now runs but require HTTPS

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,28 @@
-use teloxide::prelude::*;
+// The version of ngrok ping-pong-bot, which uses a webhook to receive updates
+// from Telegram, instead of long polling.
+
+use teloxide::{prelude::*, update_listeners::webhooks};
 
 #[tokio::main]
 async fn main() {
     pretty_env_logger::init();
-    log::info!("Starting throw dice bot...");
+    log::info!("Starting ngrok ping-pong bot...");
 
     let bot = Bot::from_env();
 
-    teloxide::repl(bot, |bot: Bot, msg: Message| async move {
-        bot.send_dice(msg.chat.id).await?;
-        Ok(())
-    })
+    let addr = ([127, 0, 0, 1], 8443).into();
+    let url = "https://139.59.198.186".parse().unwrap();
+    let listener = webhooks::axum(bot.clone(), webhooks::Options::new(addr, url))
+        .await
+        .expect("Couldn't setup webhook");
+
+    teloxide::repl_with_listener(
+        bot,
+        |bot: Bot, msg: Message| async move {
+            bot.send_message(msg.chat.id, "pong").await?;
+            Ok(())
+        },
+        listener,
+    )
     .await;
 }
